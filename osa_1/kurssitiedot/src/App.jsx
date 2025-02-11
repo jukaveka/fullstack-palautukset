f29f9fbe741494a06bb8db8a4377407435cfd453
Juho Kavekari <juhokavekari@hotmail.com>
2025-02-11 18:14:09 +0200

    Create Content component

--- osa_1/kurssitiedot/src/App.jsx
+++ osa_1/kurssitiedot/src/App.jsx
@@ -4,6 +4,22 @@ const Header = (props) => {
   )
 }
 
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -16,15 +32,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
