d5e8af9e7fc72bf1f43fc10e63dc30072d0c6902
Juho Kavekari <juhokavekari@hotmail.com>
2025-02-21 17:34:25 +0200

    Add total state variable, add average calculation to Statistics component

--- osa_1/unicafe/src/App.jsx
+++ osa_1/unicafe/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 
 const Button = ({onClick, text}) => {
-  console.log("Called event handler", onClick)
-  console.log("Button text", text)
-
   return (
     <button onClick={onClick}>
       {text}
@@ -11,11 +8,21 @@ const Button = ({onClick, text}) => {
   )
 }
 
-const Statistics = ({positive, neutral, negative}) => {
+const Statistics = ({positive, neutral, negative, total}) => {
   console.log("Rendering reactions")
   console.log("Positive reactions", positive)
   console.log("Neutral reactions", neutral)
   console.log("Negative reactions", negative)
+  console.log("Total reactions", total)
+
+  const calculateAverage = () => {
+    if (total === 0) {
+      return 0
+    }
+    const reactionValue = (positive * 1) + (negative * -1)
+
+    return reactionValue / total
+  }
 
   return (
     <div>
@@ -23,6 +30,8 @@ const Statistics = ({positive, neutral, negative}) => {
       <p> Good {positive} </p>
       <p> Neutral {neutral} </p>
       <p> Bad {negative} </p>
+      <p> Total {total} </p>
+      <p> Average {calculateAverage()} </p>
     </div>
   )
 }
@@ -31,20 +40,36 @@ const App = () => {
   const [positive, setPositive] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [negative, setNegative] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const addPositive = () => {
     console.log("Increasing count of positive reactions, original count", positive, ", new count", positive + 1)
     setPositive(positive + 1)
+
+    const newPositive = positive + 1
+
+    console.log("Increasing total reactions, original value", total, ", new total", (newPositive + neutral + negative))
+    setTotal(newPositive + neutral + negative)
   }
 
   const addNeutral = () => {
     console.log("Increasing count of neutral reactions, original count", neutral, ", new count", neutral + 1)
     setNeutral(neutral + 1)
+
+    const newNeutral = neutral + 1
+
+    console.log("Increasing total reactions, original value", total, ", new total", (positive + newNeutral + negative))
+    setTotal(positive + newNeutral + negative)
   }
 
   const addNegative = () => {
     console.log("Increasing count of negative reactions, original count", negative, ", new count", negative + 1)
     setNegative(negative + 1)
+
+    const newNegative = negative + 1
+
+    console.log("Increasing total reactions, original value", total, ", new total", (positive + neutral + newNegative))
+    setTotal(positive + neutral + newNegative)
   }
 
   return (
@@ -53,7 +78,12 @@ const App = () => {
       <Button onClick={addPositive} text="Good" />
       <Button onClick={addNeutral} text="Neutral" />
       <Button onClick={addNegative} text="Negative" />
-      <Statistics positive={positive} neutral={neutral} negative={negative} />
+      <Statistics 
+        positive={positive} 
+        neutral={neutral} 
+        negative={negative}
+        total={total} 
+      />
     </div>
   )
 }
