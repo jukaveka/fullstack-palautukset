3eaae549783d6b3ad6113bc4eae948c44ab02f75
Juho Kavekari <juhokavekari@hotmail.com>
2025-02-26 12:59:14 +0200

    Copy kurssitiedot from osa_1, create Course component

---
+++ osa_2/kurssitiedot/src/App.jsx
@@ -0,0 +1,84 @@
+const Header = (props) => {
+  console.log(props)
+  
+  return (
+    <h1> {props.course.name} </h1>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+
+  const [part1, part2, part3] = props.course.parts
+
+  console.log(part1, part2, part3)
+  
+  return (
+    <>
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+
+  const [part1, part2, part3] = props.course.parts
+
+  console.log(part1, part2, part3)
+  
+  return (
+    <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises} </p>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  
+  return (
+      <p> {props.name} {props.exercises} </p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { 
+        name: 'Fundamentals of React', 
+        exercises: 10,
+        id: 1
+      },
+      { 
+        name: 'Using props to pass data', 
+        exercises: 7,
+        id: 2
+      },
+      { 
+        name: 'State of a component', 
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
