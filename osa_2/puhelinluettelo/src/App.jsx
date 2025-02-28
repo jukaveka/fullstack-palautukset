f080b05f33c3bd52bec781a84f2c51c6d28865b9
Juho Kavekari <juhokavekari@hotmail.com>
2025-02-28 15:00:20 +0200

    Create form to add new person, mapping to display added names

---
+++ osa_2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  console.log("State variable persons", persons)
+  console.log("State variable newName", newName)
+
+  const changeNameInput = (event) => {
+    console.log("Input changed for name input element. Original value", newName, ", new value", event.target.value)
+
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log("Button clicked to add new person, value", event.target.value)
+
+    const personObject = {
+      name: newName
+    }
+
+    console.log("Person object to be added to persons", personObject)
+
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={changeNameInput} />
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+
+}
+
+export default App
