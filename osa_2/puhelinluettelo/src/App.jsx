6d39909bbd343df17d432faa73655660f240f13f
Juho Kavekari <juhokavekari@hotmail.com>
2025-03-01 17:23:41 +0200

    Move states and functions relevant onlyto adding new person under PersonForm component

--- osa_2/puhelinluettelo/src/App.jsx
+++ osa_2/puhelinluettelo/src/App.jsx
@@ -9,22 +9,8 @@ const App = () => {
     { name: 'Marko Makkara', number: '+358501234567' },
     { name: 'Piia Paju', number: '040 0987654' }
   ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
   const [showAll, setShowAll] = useState(true)
-
-  const changeNameInput = (event) => {
-    console.log("Input changed for name input element. Original value", newName, ", new value", event.target.value)
-
-    setNewName(event.target.value)
-  }
-
-  const changeNumberInput = (event) => {
-    console.log("Input changed for phone input element. Original value", newNumber, ", new value", event.target.value)
-
-    setNewNumber(event.target.value)
-  }
 
   const changeSearchInput = (event) => {
     console.log("Input changed for phone input element. Original value", search, ", new value", event.target.value)
@@ -36,28 +22,6 @@ const App = () => {
       : setShowAll(true)
   }
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    console.log("Button clicked to add new person, value", newName)
-
-    const personNames = persons.map(person => person.name )
-
-    if (personNames.includes(newName)) {
-      alert(`${newName} is already in use`)
-    } else {
-      const personObject = {
-        name: newName,
-        number: newNumber
-      }
-  
-      console.log("Person object to be added to persons", personObject)
-  
-      setPersons(persons.concat(personObject))
-      setNewName("")
-      setNewNumber("")
-    }
-  }
-
   const filteredPersons = showAll
     ? persons
     : persons.filter(person => person.name.includes(search))
@@ -66,7 +30,7 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Search search={search} onChange={changeSearchInput} />
-      <PersonForm onSubmit={addPerson} newName={newName} onNameInputChange={changeNameInput} newNumber={newNumber} onNumberInputChange={changeNumberInput} />
+      <PersonForm persons={persons} setPersons={setPersons} />
       <Directory showAll={showAll} persons={persons} search={search} />
     </div>
   )
--- osa_2/puhelinluettelo/src/components/PersonForm.jsx
+++ osa_2/puhelinluettelo/src/components/PersonForm.jsx
@@ -1,23 +1,61 @@
-const PersonForm = ({ onSubmit, newName, onNameInputChange, newNumber, onNumberInputChange }) => {
+import { useState } from 'react'
 
-    return (
-    <div>
-        <h2> Add new person to phonebook </h2>
-        <form onSubmit={onSubmit}>
-            <label htmlFor="name"> Name </label>
-            <p>
-              <input id="name" name="name" value={newName} onChange={onNameInputChange} />
-            </p>
-            <label htmlFor="number"> Phone number </label>
-            <p>
-              <input id="number" name="number" value={newNumber} onChange={onNumberInputChange} />
-            </p>
-            <p>
-              <button type="submit"> Add </button>
-            </p> 
-        </form>
-    </div>
-    )
+const PersonForm = ({ persons, setPersons }) => {
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const changeNameInput = (event) => {
+    console.log("Input changed for name input element. Original value", newName, ", new value", event.target.value)
+
+    setNewName(event.target.value)
+  }
+
+  const changeNumberInput = (event) => {
+    console.log("Input changed for phone input element. Original value", newNumber, ", new value", event.target.value)
+
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log("Button clicked to add new person, value", newName)
+
+    const personNames = persons.map(person => person.name )
+
+    if (personNames.includes(newName)) {
+      alert(`${newName} is already in use`)
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+  
+      console.log("Person object to be added to persons", personObject)
+  
+      setPersons(persons.concat(personObject))
+      setNewName("")
+      setNewNumber("")
+    }
+  }
+
+  return (
+  <div>
+      <h2> Add new person to phonebook </h2>
+      <form onSubmit={addPerson}>
+          <label htmlFor="name"> Name </label>
+          <p>
+            <input id="name" name="name" value={newName} onChange={changeNameInput} />
+          </p>
+          <label htmlFor="number"> Phone number </label>
+          <p>
+            <input id="number" name="number" value={newNumber} onChange={changeNumberInput} />
+          </p>
+          <p>
+            <button type="submit"> Add </button>
+          </p> 
+      </form>
+  </div>
+  )
 }
 
 export default PersonForm
