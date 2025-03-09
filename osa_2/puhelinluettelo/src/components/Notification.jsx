53dc6eac95b0492317bfb8083e7e79ce82d46610
Juho Kavekari <juhokavekari@hotmail.com>
2025-03-09 17:52:29 +0200

    Add styling for success notifications

--- osa_2/puhelinluettelo/src/components/Notification.jsx
+++ osa_2/puhelinluettelo/src/components/Notification.jsx
@@ -1,10 +1,20 @@
 const Notification = ({ successMessage }) => {
+  const successMessageStyle = {
+    color: "#285238",
+    fontSize: "20px",
+    fontWeight: "bold",
+    padding: "10px",
+    border: "solid",
+    borderColor: "#729EA1",
+    borderSize: "10px"
+  }
+
   if (successMessage === null) {
     return null
   }
   
   return (
-    <div>
+    <div style={successMessageStyle}>
         <p> {successMessage} </p>
     </div>
   )
