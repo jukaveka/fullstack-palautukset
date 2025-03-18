ec407ea81cf6912df83f25863be8d0c08ab5ccf1
Juho Kavekari <juhokavekari@hotmail.com>
2025-03-18 18:41:23 +0200

    Puhelinluettelo - Add check for POST request body containing required information

--- osa_3/puhelinluettelo_backend/index.js
+++ osa_3/puhelinluettelo_backend/index.js
@@ -65,6 +65,12 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons/', (request, response) => {
   const body = request.body
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number missing. Both are required for adding new person"
+    })
+  }
+
   const person = {
     name: body.name,
     number: body.number,
