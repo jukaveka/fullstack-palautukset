eae6f50b9d2ff6dadd47f5d67657273f38849bbc
Juho Kavekari <juhokavekari@hotmail.com>
2025-03-27 19:46:47 +0200

    Puhelinluettelo - Update all routes to utilize DB and error handler, add console logs

--- osa_3/puhelinluettelo_backend/index.js
+++ osa_3/puhelinluettelo_backend/index.js
@@ -21,8 +21,9 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
+app.get('/info', (request, response, next) => {
+  console.log("Fetching information of phonebook")
 
-app.get('/info', (request, response) => {
   Person
     .countDocuments({})
     .then(documentCount => {
@@ -31,31 +32,41 @@ app.get('/info', (request, response) => {
 
       return response.send(`${info} <br> <br> ${date}`)
     })
+    .catch(error => next(error))
 
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
+  console.log("Fetching all persons from phonebook")
+
   Person
     .find({})
     .then(teams => {
+      console.log("All persons fetched from phonebook, documents fetched", teams.length)
+
       return response.json(teams)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const personId = request.params.id
+app.get('/api/persons/:id', (request, response, next) => {
+  console.log("Fetching person by id", request.params.id)
 
-  const person = persons.find(person => person.id === personId)
-
-  if (person) {
-    return response.json(person)
-  } else {
-    return response.status(404).end()
-  }
-
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (person) {
+        return response.json(person)
+      } else {
+        return response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
+  console.log("Deleting of person with id", request.params.id)
+
   Person
     .findByIdAndDelete(request.params.id)
     .then(deletedPerson => {
@@ -65,10 +76,12 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
+  console.log("Adding new person to phonebook")
+
   const body = request.body
 
-  console.log("Confirming that request body contains necessary values")
+  console.log("Confirming that request body contains necessary values", request.body)
 
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -92,14 +105,56 @@ app.post('/api/persons/', (request, response) => {
       console.log("Person saved to database", savedPerson)
       response.json(savedPerson)
     })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  console.log("Updating person information, id", request.params.id)
+
+  const {name, number} = request.body
+
+  console.log("Confirming request body icludes required information")
+
+  if (!name || !number) {
+    console.log("Name or number missing from request body", name, number)
+
+    return request.status(400).json({
+      error: "Name or number missing. Both are required for adding new person"
+    })
+  }
+
+  console.log("Fetching person to be updated from database")
+
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      console.log("Person to be updated", person)
+
+      person.name = name
+      person.number = number
+
+      console.log("Person with updated data", person)
+
+      person
+        .save()
+        .then(updatedPerson => {
+          console.log("Updated person successfully", updatedPerson)
+
+          return response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+    })
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message)
+  console.log("Error while handling request", request)
+  console.log("Error message", error.message)
 
   if (error.name === "CastError") {
     return response.status(400).send({error: "Issues with format of provided ID"})
   }
+
+  next(error)
 }
 
 app.use(errorHandler)
