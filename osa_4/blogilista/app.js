7e44c20810034f2173b1ca412c7464c3d0facaf0
Juho Kavekari <juhokavekari@hotmail.com>
2025-04-29 14:40:18 +0300

    Blogilista - Move application to app module

---
+++ osa_4/blogilista/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require("./utils/config")
+const blogRouter = require("./controllers/blog")
+
+const app = express()
+app.use(express.json())
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+app.use("/api/blogs", blogRouter)
+
+module.exports = app
--- osa_4/blogilista/index.js
+++ osa_4/blogilista/index.js
@@ -1,16 +1,5 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require("./app")
 const config = require("./utils/config")
-const blogRouter = require("./controllers/blog")
-
-const app = express()
-app.use(express.json())
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-app.use("/api/blogs", blogRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
