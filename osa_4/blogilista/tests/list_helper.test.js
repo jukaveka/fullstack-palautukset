b07d77f9c180ceb4456574d82e7362fe05630918
Juho Kavekari <juhokavekari@hotmail.com>
2025-05-01 16:04:48 +0300

    Blogilista - Test total likes for empty array

--- osa_4/blogilista/tests/list_helper.test.js
+++ osa_4/blogilista/tests/list_helper.test.js
@@ -8,3 +8,13 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe("Total likes", () => {
+  const listWithoutBlogs = []
+
+  test("in list with no blogs results 0", () => {
+    const result = listHelper.totalLikes(listWithoutBlogs)
+
+    assert.strictEqual(result, 0)
+  })
+})
--- osa_4/blogilista/utils/list_helper.js
+++ osa_4/blogilista/utils/list_helper.js
@@ -2,4 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  
+  const likesReducer = (totalCount, blogLikes) => {
+    totalCount + likes
+  }
+
+  const likes = blogs.reduce(likesReducer, 0)
+
+  return likes
+}
+
+module.exports = { dummy, totalLikes }
