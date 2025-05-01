6a26719cc64a260d969be042f334646cb31b6e65
Juho Kavekari <juhokavekari@hotmail.com>
2025-05-01 22:41:06 +0300

    Blogilista - Test authors with most likes

--- osa_4/blogilista/tests/list_helper.test.js
+++ osa_4/blogilista/tests/list_helper.test.js
@@ -144,6 +144,50 @@ const listWithTiedTopLikes =
     }
   ]
 
+  const listWithTiedMostLikes =
+  [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 7,
+      __v: 0
+    }
+  ]
+
 test("dummy returns one", () => {
   const blogs = []
 
@@ -233,6 +277,45 @@ describe("Author with most blogs", () => {
     }
 
     const result = listHelper.mostBlogs(listWithTiedMostBlogs)
+
+    assert.deepStrictEqual(result, correctResult)
+  })
+})
+
+describe("Author with most blog likes", () => {
+  test("is 0 with empty array", () => {
+    const result = listHelper.mostLikes(listWithoutBlogs)
+
+    assert.strictEqual(result, 0)
+  })
+
+  test("matches likes of only blog when array has one blog", () => {
+    const correctResult = {
+      author: "Edsger W. Dijkstra",
+      likes: 5
+    }
+    const result = listHelper.mostLikes(listWithOneBlog)
+
+    assert.deepStrictEqual(result, correctResult)
+  })
+
+  test("matches likes of author with most likes when array has multiple blogs", () => {
+    const correctResult = {
+      author: "Edsger W. Dijkstra",
+      likes: 17
+    }
+    const result = listHelper.mostLikes(listWithManyBlogs)
+
+    assert.deepStrictEqual(result, correctResult)
+  })
+
+  test("matches likes of first appearing author with most likes when array has multiple authors with most likes", () => {
+    const correctResult = {
+      author: "Edsger W. Dijkstra",
+      likes: 17
+    }
+
+    const result = listHelper.mostLikes(listWithTiedMostLikes)
 
     assert.deepStrictEqual(result, correctResult)
   })
--- osa_4/blogilista/utils/list_helper.js
+++ osa_4/blogilista/utils/list_helper.js
@@ -26,25 +26,24 @@ const favouriteBlog = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 const findAuthorWithMostBlogs = (blogs) => {
-  const getName = iteratee("author")
-  const groupedAuthors = groupBy(blogs, getName)
-  
+  const groupedAuthors = groupBy(blogs, "author")
+
   const authors = Object.keys(groupedAuthors)
-  
+
   const authorsBlogCount = authors.map((author) => {
     return {
       "author": author,
       "blogs": groupedAuthors[author].length
     }
   })
-  
+
   const authorWithMostBlogs = authorsBlogCount.reduce((topAuthor, author) => {
     return topAuthor.blogs >= author.blogs
       ? topAuthor
       : author
   })
 
-return authorWithMostBlogs
+  return authorWithMostBlogs
 }
 
 
@@ -54,4 +53,52 @@ const mostBlogs = (blogs) => {
     : findAuthorWithMostBlogs(blogs)
 }
 
-module.exports = { dummy, totalLikes, favouriteBlog, mostBlogs }
+const findAuthorWithMostLikes = (blogs) => {
+  const groupedAuthors = groupBy(blogs, "author")
+
+  console.log("Object with authors and their blogs in array after grouping", groupedAuthors)
+
+  const authors = Object.keys(groupedAuthors)
+
+  console.log("Authors array", authors)
+
+  const authorsBlogLikeCount = authors.map((author) => {
+    console.log("Author during mapping of authors array", author)
+
+    const authorsBlogs = groupedAuthors[author]
+
+    console.log("The array of blogs for author", authorsBlogs)
+
+    const authorsBlogsLikes = authorsBlogs
+      .map((blog) => blog.likes)
+      .reduce((totalBlogLikes, blogLikes) => totalBlogLikes + blogLikes, 0)
+
+    console.log("Author blog likes after reduction", authorsBlogsLikes)
+
+    return {
+      "author": author,
+      "likes": authorsBlogsLikes
+    }
+  })
+
+  console.log("Array with authors and count of the likes given to their blogs after mapping", authorsBlogLikeCount)
+
+  const authorWithMostBlogLikes = authorsBlogLikeCount.reduce((topAuthor, author) => {
+    console.log("Most liked author during reduction", topAuthor, "current author", author)
+    return topAuthor.likes >= author.likes
+      ? topAuthor
+      : author
+  })
+
+  console.log("Object containing author with most likes after reduction", authorWithMostBlogLikes)
+
+  return authorWithMostBlogLikes
+}
+
+const mostLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : findAuthorWithMostLikes(blogs)
+}
+
+module.exports = { dummy, totalLikes, favouriteBlog, mostBlogs, mostLikes }
