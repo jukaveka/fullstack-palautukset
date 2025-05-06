712d72d78a15a16775dd7ebff6f8a155d22b2584
Juho Kavekari <juhokavekari@hotmail.com>
2025-05-06 17:51:08 +0300

    Blogilista - Changing test data

--- osa_4/blogilista/tests/test_blogs.js
+++ osa_4/blogilista/tests/test_blogs.js
@@ -185,10 +185,10 @@ const listWithTiedMostLikesForAuthor =
     }
   ]
 const newBlog = {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5
+    title: 'On elegance',
+    author: 'Bas Dijkstra',
+    url: 'https://www.ontestautomation.com/on-elegance/',
+    likes: 8
   }
 const newBlogWithoutLikes = {
   title: 'Go To Statement Considered Harmful',
