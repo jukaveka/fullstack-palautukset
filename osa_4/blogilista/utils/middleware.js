b9accf4e968d18b52e498ae785d2a47931be8b95
Juho Kavekari <juhokavekari@hotmail.com>
2025-05-12 18:54:41 +0300

    Blogilista - Add error handling for validation errors

--- osa_4/blogilista/utils/middleware.js
+++ osa_4/blogilista/utils/middleware.js
@@ -1,6 +1,8 @@
 const errorHandler = (error, request, response, next) => {
   if (error.name === "MongoServerError" && error.message.includes("E11000 duplicate key error")) {
     return response.status(400).json({ error: "Username already exists "})
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
