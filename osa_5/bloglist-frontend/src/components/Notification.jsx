82d6579637194ce9e2f75283f8e4133fce64ff25
Juho Kavekari <juhokavekari@hotmail.com>
2025-05-22 21:11:14 +0300

    Blogilista frontend - Create Notification component

---
+++ osa_5/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,41 @@
+const Notification = ({ successMessage, errorMessage }) => {
+  const successMessageStyle = {
+    color: "#285238",
+    fontSize: "20px",
+    fontWeight: "bold",
+    padding: "10px",
+    border: "solid",
+    borderColor: "#285238",
+    borderSize: "10px"
+  }
+
+  const errorMessageStyle = {
+    color: "#9E2B14",
+    fontSize: "20px",
+    fontWeight: "bold",
+    padding: "10px",
+    border: "solid",
+    borderColor: "#9E2B14",
+    borderSize: "10px"
+  }
+
+  if (successMessage) {
+    return (
+      <div style={successMessageStyle}>
+        <p> {successMessage} </p>
+      </div>
+    )
+  }
+
+  if (errorMessage) {
+    return (
+      <div style={errorMessageStyle}>
+        <p> {errorMessage} </p>
+      </div>
+    )
+  }
+
+  return null
+}
+
+export default Notification
