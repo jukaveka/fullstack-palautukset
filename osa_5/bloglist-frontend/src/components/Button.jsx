ece9c29394fc442cf3ba6a6736f49dd271e20b50
Juho Kavekari <juhokavekari@hotmail.com>
2025-05-22 18:32:23 +0300

    Blogilista frontend - Create Button component

---
+++ osa_5/bloglist-frontend/src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ text, onClick }) => {
+  return (
+    <>
+      <p>
+        <button onClick={onClick}>
+          {text}
+        </button>
+      </p>
+    </>
+  )
+}
+
+export default Button
