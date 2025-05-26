cfbd4f95c0456d0573752d1a2d08c16035bd6cb7
Juho Kavekari <juhokavekari@hotmail.com>
2025-05-26 18:17:53 +0300

    Blogilista FE - Configure Eslint, fix issues

--- osa_5/bloglist-frontend/.eslintrc.cjs
+++ osa_5/bloglist-frontend/.eslintrc.cjs
@@ -12,10 +12,39 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'indent': [
+      "error",
+      2
+    ],
+    'linebreak-style': [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ],
+    "arrow-spacing": [
+      "error",
+      { before: true, after: true }
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "no-console": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
-    ],
-    'react/prop-types': false
+    ]
   },
 }
