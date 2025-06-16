5456e6e44590ef65dfc57b6c660cff79aa1ee4a0
Juho Kavekari <juhokavekari@hotmail.com>
2025-06-16 19:26:46 +0300

    Redux Anecdotes - Create Anecdote component

--- osa_6/redux-anecdotes/src/components/Anecdotes.jsx
+++ osa_6/redux-anecdotes/src/components/Anecdotes.jsx
@@ -1,5 +1,26 @@
 import { useDispatch, useSelector } from "react-redux"
 import { addVote } from "../reducers/anecdoteReducer"
+import PropTypes from "prop-types"
+
+const Anecdote = ({ anecdote, onClick }) => {
+
+  return (
+    <div key={anecdote.id}>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={() => onClick(anecdote.id)}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+Anecdote.propTypes = {
+  anecdote: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired
+}
 
 const Anecdotes = () => {
   const dispatch = useDispatch()
@@ -9,27 +30,23 @@ const Anecdotes = () => {
     const votedAnecdote = anecdotes.find(anecdote => anecdote.id === id)
     const newAnecdote = {
       ...votedAnecdote,
-      votes: votedAnecdote.votes + 1 
+      votes: votedAnecdote.votes + 1
     }
     const updatedAnecdotes = anecdotes.map(anecdote =>
       anecdote.id !== id ? anecdote : newAnecdote
     )
     dispatch(addVote(updatedAnecdotes))
-}
+  }
 
   return (
     <>
       {
         anecdotes.map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
-          </div>
+          <Anecdote 
+            key={anecdote.id} 
+            anecdote={anecdote}
+            onClick={vote}
+          />
         )
       }
     </>
