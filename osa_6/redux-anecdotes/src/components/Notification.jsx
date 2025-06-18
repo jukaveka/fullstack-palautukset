50043953c04f3475ab18c768883f6252eefe83cd
Juho Kavekari <juhokavekari@hotmail.com>
2025-06-18 14:52:32 +0300

    Redux Anecdotes - Update Notification component as .jsx file, display set notification in App component

---
+++ osa_6/redux-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux"
+
+const Notification = () => {
+  const notification = useSelector(state => state.notification)
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    margin: "10px 0px"
+  }
+  return (
+    <div style={style}>
+      {notification}
+    </div>
+  )
+}
+
+export default Notification
