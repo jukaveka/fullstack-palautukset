d0ef8526b23a57eb009b6ed6af6e51064077925a
Juho Kavekari <juhokavekari@hotmail.com>
2025-06-23 18:51:14 +0300

    Query Anecdotes - Create Anecdote component

---
+++ osa_6/query-anecdotes/src/components/Anecdote.jsx
@@ -0,0 +1,16 @@
+const Anecdote = ({ anecdote, handleClick }) => {
+
+  return (
+    <div>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={() => handleClick(anecdote)}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+export default Anecdote
