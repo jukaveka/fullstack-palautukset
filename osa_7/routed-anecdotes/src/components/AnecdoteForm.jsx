15ec8c3393fcd16129b30f5678777c257c6612ac
Juho Kavekari <juhokavekari@hotmail.com>
2025-06-25 19:23:10 +0300

    Routed Anecdotes - Use useField custom hook for anecdote form

--- osa_7/routed-anecdotes/src/components/AnecdoteForm.jsx
+++ osa_7/routed-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react"
+import { useField } from "../hooks"
 
 const AnecdoteForm = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField("text")
+  const author = useField("text")
+  const info = useField("text")
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
+
+  
 
   return (
     <div>
@@ -22,15 +24,15 @@ const AnecdoteForm = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input { ...content }/>
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input { ...author } />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input { ...info } />
         </div>
         <button>create</button>
       </form>
