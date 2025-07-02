1536986fceab453624f5238417e168f63d01661a
Juho Kavekari <juhokavekari@hotmail.com>
2025-07-02 17:40:47 +0300

    Bloglist FE - Create notification reducer

---
+++ osa_7/bloglist-frontend/src/reducers/NotificationReducer.js
@@ -0,0 +1,10 @@
+const NotificationReducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_BLOG":
+      return state
+    default:
+      return state
+  }
+}
+
+export default NotificationReducer
