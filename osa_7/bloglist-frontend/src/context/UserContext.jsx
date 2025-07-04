ff98a2e4eefa41fc5d4a1f8d1cea092e9f6e0eed
Juho Kavekari <juhokavekari@hotmail.com>
2025-07-04 14:22:04 +0300

    Bloglist FE - Create user reducer and context

---
+++ osa_7/bloglist-frontend/src/context/UserContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useReducer } from "react"
+import UserReducer from "../reducers/UserReducer"
+
+const UserContext = createContext()
+
+export const UserContextProvider = (props) => {
+  const [user, userDispatch] = useReducer(UserReducer)
+
+  return (
+    <UserContext.Provider value={[user, userDispatch]}>
+      {props.children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUserDispatch = () => {
+  const userContext = useContext(UserContext)
+  return userContext[1]
+}
+
+export default UserContext
---
+++ osa_7/bloglist-frontend/src/reducers/UserReducer.js
@@ -0,0 +1,12 @@
+const UserReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return action.payload
+    case "CLEAR_USER":
+      return null
+    default:
+      return state
+  }
+}
+
+export default UserReducer
