6ab6e4e601276a0156365d2f7e82d937c492e6b4
Juho Kavekari <juhokavekari@hotmail.com>
2025-07-09 15:54:03 +0300

    Bloglist FE - Redo user view with Material UI

--- osa_7/bloglist-frontend/src/components/User.jsx
+++ osa_7/bloglist-frontend/src/components/User.jsx
@@ -1,6 +1,20 @@
 import { useQuery } from "@tanstack/react-query"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import userService from "../services/userService"
+import {
+  Box,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material"
 
 const User = () => {
   const params = useParams()
@@ -22,12 +36,62 @@ const User = () => {
 
   return (
     <div>
-      <h3> {user.name} </h3>
-      <ul>
-        {user.blogs.map((blog) => {
-          return <li key={blog.id}>{blog.title}</li>
-        })}
-      </ul>
+      <Box
+        sx={{ display: "flex", justifyContent: "center", paddingTop: "50px" }}
+      >
+        <Paper
+          elevation={1}
+          sx={{
+            minWidth: 200,
+            maxWidth: 700,
+            flexGrow: 1,
+          }}
+        >
+          <Grid
+            container
+            spacing={2}
+            sx={{
+              textAlign: "center",
+              justifyContent: "center",
+              padding: "20px",
+            }}
+          >
+            <Grid size={12}>
+              <Typography variant="h6"> {user.name} </Typography>
+            </Grid>
+            <Grid size={6}>
+              <Typography> Username</Typography>
+              <Typography variant="h6"> {user.username} </Typography>
+            </Grid>
+            <Grid size={6}>
+              <Typography> Blogs added </Typography>
+              <Typography variant="h6"> {user.blogs.length} </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell> Blogs added </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {user.blogs.map((blog) => {
+              return (
+                <>
+                  <TableRow>
+                    <TableCell>
+                      <Link to={`/blogs/${blog.id}`}>{blog.title} </Link>
+                    </TableCell>
+                  </TableRow>
+                </>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
