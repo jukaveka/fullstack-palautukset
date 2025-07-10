901d988b2b1668348e76770125e2654ce76c8115
Juho Kavekari <juhokavekari@hotmail.com>
2025-07-10 15:51:20 +0300

    Bloglist FE - Refactor login form with MUI

--- osa_7/bloglist-frontend/src/components/LoginForm.jsx
+++ osa_7/bloglist-frontend/src/components/LoginForm.jsx
@@ -1,26 +1,20 @@
-import { useState } from "react"
-import Input from "./Input"
-import Button from "./Button"
 import loginService from "../services/login"
 import blogService from "../services/BlogService"
 import { useNotificationDispatch } from "../context/NotificationContext"
+import { useUserDispatch } from "../context/UserContext"
 import {
   setErrorNotification,
   setSuccessNotification,
 } from "../reducers/NotificationReducer"
-import { useUserDispatch } from "../context/UserContext"
+
+import { useState } from "react"
+import { Box, Button, Paper, TextField, Typography } from "@mui/material"
 
 const LoginForm = () => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const userDispatch = useUserDispatch()
   const notificationDispatch = useNotificationDispatch()
-
-  const buttonStyle = {
-    color: "white",
-    backgroundColor: "#4C5C68",
-    margin: "2px",
-  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -46,24 +40,47 @@ const LoginForm = () => {
 
   return (
     <div>
-      <h2> Login </h2>
-      <form data-testid="loginform">
-        <Input
-          label="Username"
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <Input
-          label="Password"
-          type="text"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-        <br />
-        <br />
-        <Button text="Login" onClick={handleLogin} style={buttonStyle} />
-      </form>
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: "50px",
+        }}
+      >
+        <Paper
+          elevation={1}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minWidth: 200,
+            maxWidth: 400,
+            flexGrow: 1,
+            padding: "20px",
+          }}
+        >
+          <Typography variant="h6" sx={{ textAlign: "center" }}>
+            Login
+          </Typography>
+          <TextField
+            id="login-form-username"
+            label="Username"
+            variant="outlined"
+            margin="normal"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          <TextField
+            id="login-form-password"
+            label="Password"
+            variant="outlined"
+            margin="normal"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <Button onClick={handleLogin}> Submit </Button>
+        </Paper>
+      </Box>
     </div>
   )
 }
